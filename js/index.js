56ba1b42840bf6301c0314ac85da1099ba577252
devrakibulrahman <devrakibulrahman@gmail.com>
2024-09-25 19:10:45 +0600

    donate-bangladesh

--- js/index.js
+++ js/index.js
@@ -147,7 +147,7 @@ function calculateBalance(input, balance, displayAmount, location){
     const displayAmountToNum = parseFloat(displayAmountToNumValue);
     
     if(mainBalance < 0 || balanceValue < inputValue){
-        alert('you have no more enough money in your account');
+        alert('you have no more enough money in your account!');
         inputField.value = '';
         return;
     };
